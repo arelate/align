16443ff44c118b4ce2ea2e12bde49c6afdbc8501
Bogdan Brinza <Bogdan.Brinza@hey.com>
2024-05-19 16:46:24 -0700

    Refactoring get-all-pages

--- cli/get-all-pages.go
+++ cli/get-all-pages.go
@@ -2,13 +2,17 @@ package cli
 
 import (
 	"encoding/json"
-	"fmt"
+	"errors"
 	"github.com/arelate/align/paths"
 	"github.com/arelate/southern_light/ign_integration"
 	"github.com/boggydigital/kvas"
 	"github.com/boggydigital/nod"
 	"net/url"
 	"time"
+)
+
+const (
+	throttleMs = 1000
 )
 
 func GetAllPagesHandler(u *url.URL) error {
@@ -35,54 +39,12 @@ func GetAllPages(slug string, force bool) error {
 
 	pages := map[string]bool{mainPage: false}
 
-	var wikiProps ign_integration.WikiProps
-
 	for morePages(pages) {
-
 		page := nextPage(pages)
 
-		if err := GetPage(slug, page, force); err != nil {
-			return gapa.EndWithError(err)
-		}
-
-		if err := GetData(slug, page, force); err != nil {
-			return gapa.EndWithError(err)
-		}
-
-		time.Sleep(1000 * time.Millisecond)
-
-		if err := kv.IndexRefresh(); err != nil {
-			return gapa.EndWithError(err)
-		}
-
-		wikiPage, err := kv.Get(page)
+		urls, err := getUrls(kv, slug, page, force)
 		if err != nil {
 			return gapa.EndWithError(err)
-		}
-
-		if wikiPage == nil {
-			return gapa.EndWithError(fmt.Errorf("page %s not found", page))
-		}
-
-		if err := json.NewDecoder(wikiPage).Decode(&wikiProps); err != nil {
-			return gapa.EndWithError(err)
-		}
-
-		wikiPage.Close()
-
-		urls := make([]string, 0)
-		for _, he := range wikiProps.HTMLEntities() {
-			purls, err := he.PageUrls(slug)
-			if err != nil {
-				return gapa.EndWithError(err)
-			}
-			if npu := wikiProps.NextPageUrl(); npu != "" {
-				purls = append(purls, npu)
-			}
-			if ppu := wikiProps.PreviousPageUrl(); ppu != "" {
-				purls = append(purls, ppu)
-			}
-			urls = append(urls, purls...)
 		}
 
 		for _, u := range urls {
@@ -114,4 +76,54 @@ func morePages(pages map[string]bool) bool {
 		}
 	}
 	return false
+}
+
+func getUrls(kv kvas.KeyValues, slug, page string, force bool) ([]string, error) {
+	if err := GetPage(slug, page, force); err != nil {
+		return nil, err
+	}
+
+	if err := GetData(slug, page, force); err != nil {
+		return nil, err
+	}
+
+	// throttle requests
+	time.Sleep(throttleMs * time.Millisecond)
+
+	if err := kv.IndexRefresh(); err != nil {
+		return nil, err
+	}
+
+	wikiPage, err := kv.Get(page)
+	if err != nil {
+		return nil, err
+	}
+
+	if wikiPage == nil {
+		return nil, errors.New("page not found: " + page)
+	}
+
+	defer wikiPage.Close()
+
+	var wikiProps ign_integration.WikiProps
+	if err := json.NewDecoder(wikiPage).Decode(&wikiProps); err != nil {
+		return nil, err
+	}
+
+	urls := make([]string, 0)
+	for _, he := range wikiProps.HTMLEntities() {
+		purls, err := he.PageUrls(slug)
+		if err != nil {
+			return nil, err
+		}
+		if npu := wikiProps.NextPageUrl(); npu != "" {
+			purls = append(purls, npu)
+		}
+		if ppu := wikiProps.PreviousPageUrl(); ppu != "" {
+			purls = append(purls, ppu)
+		}
+		urls = append(urls, purls...)
+	}
+
+	return urls, nil
 }
